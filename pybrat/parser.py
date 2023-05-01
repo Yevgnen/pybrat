95b62685eac3db9792f47ceb5c67aae9e0930a7c
Roman Potemin <poteminr@gmail.com>
2023-05-01 21:46:01 +0700

    fix unused "encoding" parameter in _parse_ann and add pattern of spaces to be replaced with ' '

--- pybrat/parser.py
+++ pybrat/parser.py
@@ -317,14 +317,17 @@ class BratParser(object):
                     )
                 )
 
-    def _parse_ann(self, ann, encoding):
+    def _parse_ann(self, ann, encoding, spaces_pattern):
         # Parser entities and store required data for parsing relations
         # and events.
         entity_matches, relation_matches, event_matches = [], [], []
         references = collections.defaultdict(list)
 
-        with open(ann, mode="r") as f:
+        with open(ann, mode="r", encoding=encoding) as f:
             for line in f:
+                if spaces_pattern is not None:
+                    line = re.sub(spaces_pattern, ' ', line)
+
                 line = line.rstrip()
                 if not line or line.startswith("#") or self._should_ignore_line(line):
                     continue
@@ -370,12 +373,15 @@ class BratParser(object):
             "events": list(events.values()),
         }
 
-    def _parse_text(self, txt, encoding):  # pylint: disable=no-self-use
+    def _parse_text(self, txt, encoding, spaces_pattern):  # pylint: disable=no-self-use
         with open(txt, mode="r", encoding=encoding) as f:
-            return f.read()
+            text = f.read()
+            if spaces_pattern is not None:
+                text = re.sub(spaces_pattern, ' ', text)
+            return text
 
     def parse(
-        self, dirname: Union[str, bytes, os.PathLike], encoding: str = "utf-8"
+        self, dirname: Union[str, bytes, os.PathLike], encoding: str = "utf-8", spaces_pattern: Optional[re.Pattern] = None
     ) -> list[Example]:
         """Parse examples in given directory.
 
@@ -384,6 +390,7 @@ class BratParser(object):
                 containing brat examples.
             encoding (str): Encoding for reading text files and
                 ann files
+            spaces_pattern (Optional[re.Pattern]): Pattern of spaces to be replaced with a single space
 
         Returns:
             examples (list[Example]): Parsed examples.
@@ -398,8 +405,8 @@ class BratParser(object):
         )
 
         for key, (ann_file, txt_file) in file_groups:
-            txt = self._parse_text(txt_file, encoding=encoding)
-            ann = self._parse_ann(ann_file, encoding=encoding)
+            txt = self._parse_text(txt_file, encoding=encoding, spaces_pattern=spaces_pattern)
+            ann = self._parse_ann(ann_file, encoding=encoding, spaces_pattern=spaces_pattern)
             examples += [Example(text=txt, **ann, id=key)]
 
         examples.sort(key=lambda x: x.id if x.id is not None else "")
